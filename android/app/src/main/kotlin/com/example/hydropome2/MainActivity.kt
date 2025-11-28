7e589bf05223fe210f18794167dcbd49c92cbb72
UserHan9 <raihanindra712@gmail.com>
2025-11-28 13:00:40 +0700

    Resolve

---
+++ android/app/src/main/kotlin/com/example/hydropome2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hydropome2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'core/config/app_theme.dart';
+import 'core/routes/app_router.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      onGenerateRoute: AppRouter.onGenerateRoute,
+      initialRoute: AppRouter.onboarding,
+    );
+  }
+}
